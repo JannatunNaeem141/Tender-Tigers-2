45f05987e245ce9061a50aa5c9c864ea0a1d05bb
jannatunnaeem141 <jannatunnaeem141@gmail.com>
2023-09-07 00:16:03 +0600

    Responsive issues solved

--- components/Banner.js
+++ components/Banner.js
@@ -25,8 +25,8 @@ export default function Banner() {
     }
     return (
         <div className='banner-bg-img bg-center bg-no-repeat bg-cover'>
-            <div className='max-w-7xl lg:mx-auto md:mx-8 mx-5 lg:px-8'>
-                <div className='md:flex grid grid-cols-1 items-center justify-center gap-12  lg:min-h-screen md:min-h-[80vh] min-h-screen md:py-32 py-28'>
+            <div className='max-w-7xl lg:mx-auto md:mx-8 mx-5 lg:px-8 maxWidthofBanner'>
+                <div className='md:flex grid grid-cols-1 items-center justify-center lg:gap-12 md:gap-8 gap-12 lg:min-h-screen md:min-h-[80vh] min-h-screen md:py-32 py-28'>
                     <div className='md:w-[420px] w-full'>
                         <div className='left-card-img bg-cover bg-no-repeat bg-center h-72 rounded-t-2xl'></div>
                         <div className='card-inner-bg-css p-8 flex flex-col gap-4 border border-[#601FEB]'>
@@ -53,7 +53,7 @@ export default function Banner() {
                             </div>
                         </div>
                         <div className='rounded-b-2xl flex items-center justify-between overflow-hidden card-inner-bg-css2 border-b border-b-[#601FEB]'>
-                            <button onClick={handleQuantityDecrease} className={`bg-[#5C1FDF] hover:bg-[#3907a6] duration-500 px-8 py-3 ${mintQuantity <= 1 ? "opacity-40" : "opacity-100"}`}><AiOutlineMinus className='text-[#DBDFF0] text-xl' /></button>
+                            <button onClick={handleQuantityDecrease} className={`bg-[#5C1FDF] hover:bg-[#3907a6] duration-500 px-8 py-3 ${mintQuantity <= 1 ? "opacity-40" : "opacity-100"}`}><AiOutlineMinus className='text-[#DBDFF0] text-2xl' /></button>
                             <input onKeyDown={(evt) => ["e", "E", "+", "-", "/"].includes(evt.key) && evt.preventDefault()}
                                 autoFocus
                                 onChange={(e) => {
@@ -68,13 +68,13 @@ export default function Banner() {
                                 type="number" name="" id=""
                                 defaultValue={mintQuantity}
                                 key={mintQuantity}
-                                className="bg-transparent outline-none text-center text-white text-lg"
+                                className="bg-transparent outline-none text-center text-white text-xl numInput"
                             />
-                            <button onClick={handleQuantityIncrease} className={`bg-[#5C1FDF] hover:bg-[#3907a6] duration-500 px-8 py-3  ${mintQuantity === 5 ? "opacity-40" : "opacity-100"}`}><AiOutlinePlus className='text-[#DBDFF0] text-xl' /></button>
+                            <button onClick={handleQuantityIncrease} className={`bg-[#5C1FDF] hover:bg-[#3907a6] duration-500 px-8 py-3  ${mintQuantity === 5 ? "opacity-40" : "opacity-100"}`}><AiOutlinePlus className='text-[#DBDFF0] text-2xl' /></button>
                         </div>
                     </div>
                     <div className='md:w-[420px] w-full'>
-                        <div className='flex flex-col lg:gap-8 gap-5'>
+                        <div className='flex flex-col'>
                             <div className='flex items-center border border-[#601FEB] w-full right-card-top-bg'>
                                 <div className='flex items-center justify-between p-3 border-r border-r-[#601FEB] w-1/2'>
                                     <p className='text-[#DBDFF0] text-base font-medium'>Whitelist</p>
@@ -87,13 +87,13 @@ export default function Banner() {
                                     <p className='text-[#DBDFF0] text-base'>0/50</p>
                                 </div>
                             </div>
-                            <h1 className='text-white md:text-4xl text-2xl uppercase font-bold'>Public mint <span className='text-[#1BF1B1]'>live</span></h1>
+                            <h1 className='text-white md:text-4xl text-2xl uppercase font-bold lg:my-8 my-5'>Public mint <span className='text-[#1BF1B1]'>live</span></h1>
                             <p className='text-[#939FD3] text-base'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Obcaecati unde at odit laudantium.</p>
-                            <h3 className='text-2xl uppercase font-semibold text-[#FDBE44]'>Public mint end in</h3>
+                            <h3 className='text-xl uppercase font-semibold text-[#FDBE44] lg:mt-8 mt-5 lg:mb-5 mb-3'>Public mint end in</h3>
                             <div className='flex'>
                                 <div className='text-xl px-3 py-2 text-[#FF434E] border border-[#FF455042] flex items-center gap-3 warningTextBg'><PiWarning />This auction has ended</div>
                             </div>
-                            <ul>
+                            <ul className='lg:my-8 my-5'>
                                 <li className='text-[#DBDFF0] uppercase text-base flex items-center gap-3'><MdOutlineRadioButtonUnchecked className="text-[#FECF1E]" />Max 5 nfts per wallet</li>
                                 <li className='text-[#DBDFF0] uppercase text-base flex items-center gap-3'><MdOutlineRadioButtonUnchecked className="text-[#FECF1E]" />Price 0.75 Matic + Gas</li>
                             </ul>
--- components/Navbar.js
+++ components/Navbar.js
@@ -1,85 +1,140 @@
-import Link from 'next/link';
-import React, { useState } from 'react';
-import { HiMenu } from 'react-icons/hi';
-import { AiOutlineClose } from 'react-icons/ai';
+// import Link from 'next/link';
+// import React, { useState } from 'react';
+// import { HiMenu } from 'react-icons/hi';
+// import { AiOutlineClose } from 'react-icons/ai';
+// import Logo from '../public/Logos/logo.png';
+// import Image from 'next/image';
+// import { FaDiscord, FaTwitter } from 'react-icons/fa';
+// import WIcon from '../public/icons/w-icon.png';
+
+// export default function Navbar() {
+//     const [open, setOpen] = useState(false);
+//     return (
+//         <nav className={`absolute z-10 top-0 w-full py-1.5 ${open ? 'bg-[#0E102B]' : 'bg-transparent'}`}>
+//             <div className="justify-between px-4 lg:mx-auto md:mx-5 mx-0 lg:max-w-7xl lg:items-center lg:flex lg:px-8">
+//                 <div className="flex items-center justify-between py-3 lg:py-5 lg:block">
+//                     <Link href='/'><Image src={Logo} width={100} height={100} alt='Logo' /></Link>
+//                     <div className="lg:hidden flex items-center gap-3">
+//                         {/* Hamburger toggle button */}
+//                         <button className="p-2 text-gray-700 rounded-md outline-none" onClick={() => setOpen(!open)} >
+//                             {open ? (
+//                                 <svg
+//                                     xmlns="http://www.w3.org/2000/svg"
+//                                     className="w-6 h-6 dark:text-white"
+//                                     viewBox="0 0 20 20"
+//                                     fill="currentColor"
+//                                 >
+//                                     <path
+//                                         fillRule="evenodd"
+//                                         d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+//                                         clipRule="evenodd"
+//                                     />
+//                                 </svg>
+//                             ) : (
+//                                 <svg
+//                                     xmlns="http://www.w3.org/2000/svg"
+//                                     className="w-6 h-6 dark:text-white"
+//                                     fill="none"
+//                                     viewBox="0 0 24 24"
+//                                     stroke="currentColor"
+//                                     strokeWidth={2}
+//                                 >
+//                                     <path
+//                                         strokeLinecap="round"
+//                                         strokeLinejoin="round"
+//                                         d="M4 6h16M4 12h16M4 18h16"
+//                                     />
+//                                 </svg>
+//                             )}
+//                         </button>
+//                     </div>
+//                 </div>
+//                 <div>
+//                     <div className={`lg:flex lg:items-center lg:justify-between lg:pb-0 pb-12 absolute lg:static lg:bg-transparent bg-[#0E102B] lg:z-auto z-[-1] left-0 w-full lg:w-auto lg:pl-0 pl-9 transition-all duration-500 ease-in ${open ? 'top-20 ' : 'top-[-490px]'}`}>
+//                         {/* <nav className="lg:flex grid grid-cols-1 items-center justify-center lg:gap-5 gap-5 lg:pt-0 pt-8">
+//                             <Link href='/' className='text-white hover:text-[#1BF1B1] duration-500 font-medium'>Home</Link>
+//                             <Link href='#' className='text-white hover:text-[#1BF1B1] duration-500 font-medium'>Marketplace</Link>
+//                             <Link href='#' className='text-white hover:text-[#1BF1B1] duration-500 font-medium'>Collection</Link>
+//                             <Link href='#' className='text-white hover:text-[#1BF1B1] duration-500 font-medium'>Community</Link>
+//                             <Link href='#' className='text-white hover:text-[#1BF1B1] duration-500 font-medium'>Create</Link>
+//                         </nav> */}
+
+//                         <div className="mt-8 lg:hidden inline-block">
+//                             <ul className='flex items-center gap-4'>
+//                                 <Link href='#' className='p-2 border border-[#5C1FDF] bg-[#5C1FDF] hover:bg-transparent duration-500 rounded-full'><FaDiscord className='text-white text-lg' /></Link>
+//                                 <Link href='#' className='p-2 border border-[#5C1FDF] bg-[#5C1FDF] hover:bg-transparent duration-500 rounded-full'><FaTwitter className='text-white text-lg' /></Link>
+//                                 <Link href='#' className='text-white bg-[#5C1FDF] hover:bg-transparent duration-500 border border-[#5C1FDF] rounded-full py-2 px-8 font-medium flex items-center gap-2'><Image src={WIcon} alt='W-Icon' />Connect Wallet</Link>
+//                             </ul>
+//                         </div>
+//                     </div>
+//                 </div>
+//                 <div className='flex items-center lg:gap-5 gap-5'>
+//                     <div className="space-x-2 lg:inline-block hidden">
+//                         <ul className='flex items-center gap-4'>
+//                             <Link href='#' className='p-3 border border-[#5C1FDF] bg-[#5C1FDF] hover:bg-transparent duration-500 rounded-full'><FaDiscord className='text-white text-lg' /></Link>
+//                             <Link href='#' className='p-3 border border-[#5C1FDF] bg-[#5C1FDF] hover:bg-transparent duration-500 rounded-full'><FaTwitter className='text-white text-lg' /></Link>
+//                             <Link href='#' className='text-white bg-[#5C1FDF] hover:bg-transparent duration-500 border border-[#5C1FDF] rounded-full py-3 px-10 font-medium flex items-center gap-2'><Image src={WIcon} alt='W-Icon' /> Connect Wallet</Link>
+//                         </ul>
+//                     </div>
+//                 </div>
+
+//             </div>
+//         </nav>
+//     )
+// }
+
+
+
+import React from "react";
 import Logo from '../public/Logos/logo.png';
-import Image from 'next/image';
-import { FaDiscord, FaTwitter } from 'react-icons/fa';
+import { Header, Container, Group, Burger, Transition, Divider, Box, Button } from "@mantine/core";
+import { useDisclosure, useHeadroom, useWindowScroll } from "@mantine/hooks";
+import Link from "next/link";
+import Image from "next/image";
+// import { links } from "../data/NavbarData";
 import WIcon from '../public/icons/w-icon.png';
+import { FaDiscord, FaTwitter } from 'react-icons/fa';
 
+const HeaderView = () => {
+    const [opened, { toggle }] = useDisclosure(false);
 
-export default function Navbar() {
-    const [open, setOpen] = useState(false);
     return (
-        <nav className={`absolute z-10 top-0 w-full py-1.5 ${open ? 'bg-[#0E102B]' : 'bg-transparent'}`}>
-            <div className="justify-between px-4 lg:mx-auto md:mx-5 mx-0 lg:max-w-7xl lg:items-center lg:flex lg:px-8">
-                <div className="flex items-center justify-between py-3 lg:py-5 lg:block">
-                    <Link href='/'><Image src={Logo} width={100} height={100} alt='Logo' /></Link>
-                    <div className="lg:hidden flex items-center gap-3">
-                        {/* Hamburger toggle button */}
-                        <button className="p-2 text-gray-700 rounded-md outline-none" onClick={() => setOpen(!open)} >
-                            {open ? (
-                                <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    className="w-6 h-6 dark:text-white"
-                                    viewBox="0 0 20 20"
-                                    fill="currentColor"
-                                >
-                                    <path
-                                        fillRule="evenodd"
-                                        d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                                        clipRule="evenodd"
-                                    />
-                                </svg>
-                            ) : (
-                                <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    className="w-6 h-6 dark:text-white"
-                                    fill="none"
-                                    viewBox="0 0 24 24"
-                                    stroke="currentColor"
-                                    strokeWidth={2}
-                                >
-                                    <path
-                                        strokeLinecap="round"
-                                        strokeLinejoin="round"
-                                        d="M4 6h16M4 12h16M4 18h16"
-                                    />
-                                </svg>
-                            )}
-                        </button>
-                    </div>
-                </div>
-                <div>
-                    <div className={`lg:flex lg:items-center lg:justify-between lg:pb-0 pb-12 absolute lg:static lg:bg-transparent bg-[#0E102B] lg:z-auto z-[-1] left-0 w-full lg:w-auto lg:pl-0 pl-9 transition-all duration-500 ease-in ${open ? 'top-20 ' : 'top-[-490px]'}`}>
-                        {/* <nav className="lg:flex grid grid-cols-1 items-center justify-center lg:gap-5 gap-5 lg:pt-0 pt-8">
-                            <Link href='/' className='text-white hover:text-[#1BF1B1] duration-500 font-medium'>Home</Link>
-                            <Link href='#' className='text-white hover:text-[#1BF1B1] duration-500 font-medium'>Marketplace</Link>
-                            <Link href='#' className='text-white hover:text-[#1BF1B1] duration-500 font-medium'>Collection</Link>
-                            <Link href='#' className='text-white hover:text-[#1BF1B1] duration-500 font-medium'>Community</Link>
-                            <Link href='#' className='text-white hover:text-[#1BF1B1] duration-500 font-medium'>Create</Link>
-                        </nav> */}
+        <Header height={110} px="md" className={`z-50 py-3 px-0 absolute w-full top-0 -translate-y-0 border-none ${opened ? 'bg-[#0E102B]' : 'bg-transparent'}`} >
+            <Container className="flex justify-between items-center h-full lg:px-12" size="xl">
+                <Box>
+                    <Link href="/" className="flex justify-start items-center">
+                        <Image src={Logo} width={100} height={100} alt='Logo' />
+                    </Link>
+                </Box>
 
-                        <div className="mt-8 lg:hidden inline-block">
-                            <ul className='flex items-center gap-4'>
+                <ul className='hidden md:flex items-center gap-4'>
+                    <Link href='#' className='p-3 border border-[#5C1FDF] bg-[#5C1FDF] hover:bg-transparent duration-500 rounded-full'><FaDiscord className='text-white text-lg' /></Link>
+                    <Link href='#' className='p-3 border border-[#5C1FDF] bg-[#5C1FDF] hover:bg-transparent duration-500 rounded-full'><FaTwitter className='text-white text-lg' /></Link>
+                    <Link href='#' className='text-white bg-[#5C1FDF] hover:bg-transparent duration-500 border border-[#5C1FDF] rounded-full py-3 px-10 font-medium flex items-center gap-1'><Image src={WIcon} alt='W-Icon' /> Connect Wallet</Link>
+                </ul>
+
+                <Burger
+                    opened={opened}
+                    onClick={toggle}
+                    className="md:hidden"
+                    color="#fff"
+                    size="md"
+                />
+                {/* Phone */}
+                <Transition transition="pop-top-right" duration={200} mounted={opened}>
+                    {(styles) => (
+                        <Box className="absolute bg-[#0E102B] left-0 w-full z-10 overflow-hidden md:hidden rounded-none py-10 overflow-y-auto" top={110} style={styles}>
+                            <ul className='flex items-center gap-4 ml-8'>
                                 <Link href='#' className='p-2 border border-[#5C1FDF] bg-[#5C1FDF] hover:bg-transparent duration-500 rounded-full'><FaDiscord className='text-white text-lg' /></Link>
                                 <Link href='#' className='p-2 border border-[#5C1FDF] bg-[#5C1FDF] hover:bg-transparent duration-500 rounded-full'><FaTwitter className='text-white text-lg' /></Link>
-                                <Link href='#' className='text-white bg-[#5C1FDF] hover:bg-transparent duration-500 border border-[#5C1FDF] rounded-full py-2 px-8 font-medium flex items-center gap-2'><Image src={WIcon} alt='W-Icon' />Connect Wallet</Link>
+                                <Link href='#' className='text-white bg-[#5C1FDF] hover:bg-transparent duration-500 border border-[#5C1FDF] rounded-full py-2 px-8 font-medium flex items-center gap-1'><Image src={WIcon} alt='W-Icon' /> Connect Wallet</Link>
                             </ul>
-                        </div>
-                    </div>
-                </div>
-                <div className='flex items-center lg:gap-5 gap-5'>
-                    <div className="space-x-2 lg:inline-block hidden">
-                        <ul className='flex items-center gap-4'>
-                            <Link href='#' className='p-3 border border-[#5C1FDF] bg-[#5C1FDF] hover:bg-transparent duration-500 rounded-full'><FaDiscord className='text-white text-lg' /></Link>
-                            <Link href='#' className='p-3 border border-[#5C1FDF] bg-[#5C1FDF] hover:bg-transparent duration-500 rounded-full'><FaTwitter className='text-white text-lg' /></Link>
-                            <Link href='#' className='text-white bg-[#5C1FDF] hover:bg-transparent duration-500 border border-[#5C1FDF] rounded-full py-3 px-10 font-medium flex items-center gap-2'><Image src={WIcon} alt='W-Icon' /> Connect Wallet</Link>
-                        </ul>
-                    </div>
-                </div>
+                        </Box>
+                    )}
+                </Transition>
+            </Container>
+        </Header>
+    );
+};
 
-            </div>
-        </nav>
-    )
-}
+export default HeaderView;
